e7e155453cf3d23d368f4613d91ac2cb9c9fe3cc
liuxianglong_mac <liu_xianglong@live.com>
2022-11-17 21:41:38 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,15 +19,30 @@ release = '1.5.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.viewcode',
+  'sphinx.ext.todo',
+  'sphinx.ext.mathjax',
+  'sphinx.ext.apidoc',
+  'sphinx.ext.extlinks',
+  'nbsphinx',
+  'sphinx_markdown_tables'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh_CN'
 
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme  # 添加这行
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
