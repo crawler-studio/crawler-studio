ca99c1523a8a4ea872e7c1fefa950c6578148e86
liuxianglong_mac <liu_xianglong@live.com>
2022-11-17 19:07:48 +0800

    add docs folder

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+  '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
+project = 'crawler_studio'
+copyright = '2022, liuxianglong'
+author = 'liuxianglong'
+release = '1.5.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
